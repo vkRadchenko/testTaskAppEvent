31e6f4d5b52a37dd3345e72fb3e9eb0d071d2a7f
Vladimir Radchenko <vk.rad4enko@gmail.com>
2023-11-11 15:19:07 +0300

    feat: implement routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/header/header'
+import Cart from './layout/cart'
+import Catalog from './layout/catalog'
 import Main from './layout/main'
+import NotFoundPage from './layout/404/notFoundPage'
 
 function App() {
   return (
     <>
-      <Main />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   )
 }
